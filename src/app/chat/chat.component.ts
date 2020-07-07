06eb48596814347600cf66def6efdab84240c560
Alejandro Bohorquez <sergio.bohorquez@mail.escuelaing.edu.co>
2020-07-07 01:53:26 -0500

    gitflow-feature-stash: ChatComponent

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {Client} from '@stomp/stompjs';
+import * as SockJS from 'sockjs-client';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+  private client: Client;
+  conectado:boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.client = new Client();
+    this.client.webSocketFactory = ()=>{
+      return new SockJS("http://localhost:8080/chat-websocket");
+    }
+    this.client.onConnect = (frame) =>{
+      console.log('Conectados: ' + this.client.connected + ':' + frame);
+      this.conectado=true;
+    }
+    this.client.onDisconnect = (frame) =>{
+      console.log('Desconectados: ' + !this.client.connected + ':' + frame);
+      this.conectado=false;
+
+    }
+
+  }
+  conectar():void{
+    this.client.activate();
+  }
+  desconectar():void{
+    this.client.deactivate();
+  }
+
+}
